f17dacfbbcbf5a35d8643bc411e6c865e104474a
digital_goose <usliss12@yandex.ru>
2023-05-24 12:42:28 +0300

    feat: add localization

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -4,6 +4,10 @@ export default {
 
     array: 'The :attribute field is not array.',
 
-    enums: 'The :attribute field not in exist enums.'
+    enums: 'The :attribute field not in exist enums.',
+
+    number: 'The :attribute field is not a number.',
+    
+    string: 'The :attribute field is not a string.',
 
 };
